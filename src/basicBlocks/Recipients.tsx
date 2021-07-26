3861304a2e59589ab729f913b9ee4fa1b6f36906
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-07-26 17:56:33 +0300

    Redesigned lists

--- src/basicBlocks/Recipients.tsx
+++ src/basicBlocks/Recipients.tsx
@@ -7,31 +7,27 @@ export default function Recipients() {
         Для кого этот продукт?
       </div>
       <ul className="recipients__list list-group list-group-flush">
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто очень увлекается психологией
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто хочет расширить границы своего мышления
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто погряз в рутине и почти не испытывает положительных эмоций
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, у кого есть множество неразрешенных проблем, которые тянутся уже долгое время
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто наконец хочет что-то изменить в своей жизни
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто желает глубже понимать аспекты нашей жизни
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто стремится быть лучше и улучшать свою жизнь
-        </li>
-        <li className="recipients__list-item list-group-item">
-          Для тех, кто хочет работать над собой
-        </li>
+        <ListItem text={'Для тех, кто очень увлекается психологией'}/>
+        <ListItem text={'Для тех, кто хочет расширить границы своего мышления'}/>
+        <ListItem text={'Для тех, кто погряз в рутине и почти не испытывает положительных эмоций'}/>
+        <ListItem text={'Для тех, у кого есть множество неразрешенных проблем, которые тянутся уже долгое время'}/>
+        <ListItem text={'Для тех, кто наконец хочет что-то изменить в своей жизни'}/>
+        <ListItem text={'Для тех, кто желает глубже понимать аспекты нашей жизни'}/>
+        <ListItem text={'Для тех, кто стремится быть лучше и улучшать свою жизнь'}/>
+        <ListItem text={'Для тех, кто хочет работать над собой'}/>
       </ul>
     </div>
+  )
+}
+
+interface ListItemInfo {
+  text: string
+}
+
+function ListItem({text}: ListItemInfo) {
+  return (
+    <li className="recipients__list-item list-group-item">
+      {text}
+    </li>
   )
 }
