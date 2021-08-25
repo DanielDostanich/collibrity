0059935c15849d1fb516d7ea68152744c3c5a7cf
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-08-25 05:13:37 +0300

    About minifix

--- src/basicBlocks/About.tsx
+++ src/basicBlocks/About.tsx
@@ -6,15 +6,14 @@ import pict2 from '../assets/picts/pict2.png'
 export default function About() {
   return (
     <section className="about">
-      <div className="about__container">
-        <div className="about__title">
-          О ЧЕМ ГАЙД?
-        </div>
-        <div className="about__text">
-          <img src={pict2} alt="" className="about__pict" />
-          <div className="about__p">
-            <p>Гайд «С меня хватит!» - является концентратом знаний, само название говорит о том, что этот продукт - отправной шаг, после которого жизнь точно не будет прежней, так как начнёт меняться представление о многих сферах в вашей жизни.</p>
-          </div>
+
+      <div className="about__title">
+        О ЧЕМ ГАЙД?
+      </div>
+      <div className="about__text">
+        <img src={pict2} alt="" className="about__pict" />
+        <div className="about__p">
+          <p>Гайд «С меня хватит!» - является концентратом знаний, само название говорит о том, что этот продукт - отправной шаг, после которого жизнь точно не будет прежней, так как начнёт меняться представление о многих сферах в вашей жизни.</p>
         </div>
       </div>
     </section>
