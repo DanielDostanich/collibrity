40d0f6e13a7b56fe07ed832c2a0c39f2926b20e8
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-08-25 05:07:25 +0300

    About finished

--- src/basicBlocks/About.tsx
+++ src/basicBlocks/About.tsx
@@ -3,25 +3,18 @@ import React from "react"
 // @ts-ignore
 import pict2 from '../assets/picts/pict2.png'
 
-// @ts-ignore
-import pict3 from '../assets/picts/pict3.png'
-
 export default function About() {
   return (
     <section className="about">
-      <div className="about__container container">
+      <div className="about__container">
         <div className="about__title">
           О ЧЕМ ГАЙД?
         </div>
         <div className="about__text">
-          <img src={pict2} alt="" className="about__pict1" />
-          <div className="about__p1">
-            <p>Этот гайд включает в себя основную и фундаментальную информацию, которая поможет вам иначе взглянуть на свою жизнь, увидеть все не удовлетворяющие вас моменты и изменить их.</p>
+          <img src={pict2} alt="" className="about__pict" />
+          <div className="about__p">
+            <p>Гайд «С меня хватит!» - является концентратом знаний, само название говорит о том, что этот продукт - отправной шаг, после которого жизнь точно не будет прежней, так как начнёт меняться представление о многих сферах в вашей жизни.</p>
           </div>
-          <div className="about__p2">
-            <p>Гайд «С меня хватит!» является концентратом знаний, которые проверены на личном опыте и действительно работают. Само название говорит о том, что этот продукт – отправной шаг, после которого жизнь точно не будет прежней, так как начнёт меняться представление о многих сферах вашей жизни.</p>
-          </div>
-          <img src={pict3} alt="" className="about__pict2" />
         </div>
       </div>
     </section>
